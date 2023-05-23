591d81fbcea88211f1ddb169c0367effe16e3a63
egonixaimgod <bodnarbence2002@gmail.com>
2023-05-23 16:10:43 +0200

    friss

--- src/dolgozat3.java
+++ src/dolgozat3.java
@@ -1,3 +1,12 @@
+/*
+* File: dolgozat3.java
+* Author: Róth József
+* Copyright: 2023, Róth József
+* Group: Szoft I/1/N
+* Date: 2023-05-23
+* Github: https://github.com/HyBerTop/dolgozat3
+*/
+
 import javax.swing.*;
 import java.awt.*;
 import java.io.BufferedReader;
